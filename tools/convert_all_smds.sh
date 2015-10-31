b59095c970cf7397e57ba8cc1dc282154d9e161a
abainbridge <2005@deadfrog.co.uk>
2015-10-31 14:40:35 +0000

    Fixed an oops

--- tools/convert_all_smds.sh
+++ tools/convert_all_smds.sh
@@ -4,5 +4,5 @@ cd ../assets/shapes
 for i in *.smd; do 
     shp=../../data/shapes/`echo $i | sed 's/\.smd/\.shp/'`
     echo Converting $i to $shp
-    python ../../tools/smd_to_shp2.py $i $shp
+    python ../../tools/smd_to_shp.py $i $shp
 done
