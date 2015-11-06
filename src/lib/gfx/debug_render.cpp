5ad7eaf7a585d27be6d2afa61354927956bad62e
abainbridge <2005@deadfrog.co.uk>
2015-11-06 09:48:45 +0000

    Added line rendering support to the debug renderer.

--- src/lib/gfx/debug_render.cpp
+++ src/lib/gfx/debug_render.cpp
@@ -14,7 +14,15 @@ struct SphereParams
 };
 
 
+struct LineParams
+{
+    Vector3 start, end;
+    RgbaColour col;
+};
+
+
 static PoolAllocator <SphereParams> g_spheres;
+static PoolAllocator <LineParams> g_lines;
 
 
 void DebugRenderSphere(Vector3 const &pos, float radius, RgbaColour const &col)
@@ -23,6 +31,15 @@ void DebugRenderSphere(Vector3 const &pos, float radius, RgbaColour const &col)
     s->pos = pos;
     s->radius = radius;
     s->col = col;
+}
+
+
+void DebugRenderLine(Vector3 const &start, Vector3 const &end, RgbaColour const &col /* = g_colourWhite */)
+{
+    LineParams *l = g_lines.GetFree();
+    l->start = start;
+    l->end = end;
+    l->col = col;
 }
 
 
@@ -41,12 +58,29 @@ void DebugRender()
             sphereRenderer.Render(s->pos, s->radius);
         }
     }
+
+    glLineWidth(1.0f);
+
+    for (int i = 0; i < g_lines.Size(); i++)
+    {
+        if (g_lines.IsIndexUsed(i))
+        {
+            LineParams *l = g_lines.GetPointer(i);
+            glColor3ubv(l->col.GetData());
+
+            glBegin(GL_LINES);
+            glVertex3fv(l->start.GetData());
+            glVertex3fv(l->end.GetData());
+            glEnd();
+        }
+    }
 }
+
 
 void DebugRenderClear()
 {
     for (int i = 0; i < g_spheres.Size(); i++)
-    {
         g_spheres.MarkNotUsed(i);
-    }
+    for (int i = 0; i < g_lines.Size(); i++)
+        g_lines.MarkNotUsed(i);
 }
--- src/lib/gfx/debug_render.h
+++ src/lib/gfx/debug_render.h
@@ -5,9 +5,10 @@
 #include "lib/vector3.h"
 
 
-void DebugRenderSphere(Vector3 const &centre, float radius, RgbaColour const &col=RgbaColour(255,255,255));
+void DebugRenderSphere(Vector3 const &centre, float radius, RgbaColour const &col = g_colourWhite);
+void DebugRenderLine(Vector3 const &start, Vector3 const &end, RgbaColour const &col = g_colourWhite);
+
 void DebugRender();
 void DebugRenderClear();
-
 
 #endif
